5cca3daf3a4ee6908b49010a98e9789b5990c896
Pranav Shenoy <pranav.shenoy@utah.edu>
2023-02-07 14:44:27 -0800

    mnr

--- nightly-run.sh
+++ nightly-run.sh
@@ -10,53 +10,54 @@ mem_balancer_dir=$PWD
 cd $mem_balancer_dir
 
 echo "** pulling depot_tools if not present **"
-if [ ! -d  ../depot_tools ] && git clone https://chromium.googlesource.com/chromium/tools/depot_tools
+cd ../
+if [ ! -d  depot_tools ] && git clone https://chromium.googlesource.com/chromium/tools/depot_tools
 
 export PATH="$PWD/../depot_tools:$PATH"
 # ./clean_log
 # ./clean_out
-echo "** Pulling latest changes in MemoryBalancer and v8 **"
-git submodule init
-git submodule update
-git submodule sync
-echo "** pulling changes in MemoryBalancer"
+# echo "** Pulling latest changes in MemoryBalancer and v8 **"
+# git submodule init
+# git submodule update
+# git submodule sync
+# echo "** pulling changes in MemoryBalancer"
 
-cd $mem_balancer_dir
-cd ../
-echo "** pulling changes in v8 **"
-if [ ! -d  v8 ] && ./fetch.sh
-
-cd $mem_balancer_dir
-cd ../
-echo "** pulling Webkit if not present**"
-if [ ! -d  WebKit ] && git clone git@github.com:WebKit/WebKit.git
-
-cd $mem_balancer_dir
-cd ../v8/src
-git stash
-git checkout STABLE
-git pull origin STABLE
-gclient sync -f --no-history
-
-
-# echo "** cloning membalancer-paper **"
 # cd $mem_balancer_dir
-# cd "../"
-# [ ! -d "membalancer-paper" ] && git clone git@github.com:cputah/membalancer-paper.git
+# cd ../
+# echo "** pulling changes in v8 **"
+# if [ ! -d  v8 ] && ./fetch.sh
 
-cd $mem_balancer_dir
-echo "** building v8 **"
-make v8
-echo "** building memorybalancer **"
-make
+# cd $mem_balancer_dir
+# cd ../
+# echo "** pulling Webkit if not present**"
+# if [ ! -d  WebKit ] && git clone git@github.com:WebKit/WebKit.git
+
+# cd $mem_balancer_dir
+# cd ../v8/src
+# git stash
+# git checkout STABLE
+# git pull origin STABLE
+# gclient sync -f --no-history
 
 
-# echo "** running eval **"
-# python3 python/eval.py "jetstream"
-# python3 python/eval.py "acdc"
-# python3 python/gen.py --action=open
-# echo "** uploading results **"
-# result_dir=`ls "out" | sort -r | head -1`
-# if command -v nightly-results &>/dev/null; then
-#     nightly-results url "http://membalancer.uwplse.org/$result_dir"
-# fi
+# # echo "** cloning membalancer-paper **"
+# # cd $mem_balancer_dir
+# # cd "../"
+# # [ ! -d "membalancer-paper" ] && git clone git@github.com:cputah/membalancer-paper.git
+
+# cd $mem_balancer_dir
+# echo "** building v8 **"
+# make v8
+# echo "** building memorybalancer **"
+# make
+
+
+# # echo "** running eval **"
+# # python3 python/eval.py "jetstream"
+# # python3 python/eval.py "acdc"
+# # python3 python/gen.py --action=open
+# # echo "** uploading results **"
+# # result_dir=`ls "out" | sort -r | head -1`
+# # if command -v nightly-results &>/dev/null; then
+# #     nightly-results url "http://membalancer.uwplse.org/$result_dir"
+# # fi
