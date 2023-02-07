bf7d806e71e5893dd491f7dc32a811a1a532b9c2
Pranav Shenoy <pranav.shenoy@utah.edu>
2023-02-07 15:03:40 -0800

    mnr

--- nightly-run.sh
+++ nightly-run.sh
@@ -9,21 +9,22 @@ mkdir -p log
 mem_balancer_dir=$PWD
 cd $mem_balancer_dir
 
-# if [ ! -d  "$mem_balancer_dir/../depot_tools" ]; then 
-#     echo "Pulling depot_tools"
-#     cd ../
-#     git clone https://chromium.googlesource.com/chromium/tools/depot_tools
-# else
-#     echo "depot_tool exists"
-# fi
-export PATH="$PWD/depot_tools:$PATH"
+if [ ! -d  "$mem_balancer_dir/../depot_tools" ]; then 
+    echo "Pulling depot_tools"
+    cd ../
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools
+else
+    echo "depot_tool exists"
+fi
+cd $mem_balancer_dir
+export PATH="$PWD/../depot_tools:$PATH"
 # # ./clean_log
 # # ./clean_out
 # cd $mem_balancer_dir
-# echo "** Pulling submodules **"
-# git submodule init
-# git submodule update
-# git submodule sync
+echo "** Pulling submodules **"
+git submodule init
+git submodule update
+git submodule sync
 
 cd $mem_balancer_dir
 
@@ -35,7 +36,7 @@ if [ ! -d  "$mem_balancer_dir/../v8" ]; then
 else 
     echo "v8 already present"
 fi
-# cd $mem_balancer_dir
+cd $mem_balancer_dir
 # cd ../
 # echo "** pulling Webkit if not present**"
 # if [ ! -d  WebKit ] && git clone git@github.com:WebKit/WebKit.git
