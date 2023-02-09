ad2771c01541a3010fafb9f747a3e637b892eba2
Pranav Shenoy <pranav.shenoy@utah.edu>
2023-02-09 13:54:11 -0800

    mnr

--- nightly-run.sh
+++ nightly-run.sh
@@ -67,7 +67,7 @@ cd $deps_par_dir/v8/src/
 git stash
 git checkout origin/2020-12-24
 git pull origin 2020-12-24
-gclient sync -f --no-history
+# gclient sync -f --no-history
 
 # pip install ninja
 cd $mem_balancer_dir
