63a951651700181703990c6462d7b7a2e900f93f
Pranav Shenoy <pranav.shenoy@utah.edu>
2023-02-15 01:35:56 -0800

    mnr

--- python/eval.py
+++ python/eval.py
@@ -10,10 +10,15 @@ from git_check import get_commit
 from util import tex_def, tex_fmt
 import paper
 from EVAL import *
+import glob
 
-assert len(sys.argv) == 2
+assert len(sys.argv) == 3
 mode = sys.argv[1]
 assert mode in ["jetstream", "browseri", "browserii", "browseriii", "acdc", "all", "macro"]
+
+root_dir = sys.argv[2]
+
+
 
 BASELINE = {
     "BALANCE_STRATEGY": "ignore",
@@ -89,39 +94,118 @@ if mode in ["acdc", "all"]:
     evaluation.append(QUOTE(eval_acdc))
 
 # TODO:  PRANAV remove
-subprocess.run("make", shell=True)
+# subprocess.run("make", shell=True)
 
-def run(config, in_path):
-    def make_path():
-        path = in_path.joinpath(time.strftime("%Y-%m-%d-%H-%M-%S"))
-        path.mkdir()
-        with open(path.joinpath("cfg"), "w") as f:
-            f.write(str(config))
-        return path
-    if has_meta(config):
-        path = make_path()
-        for x in strip_quote(flatten_nondet(config)).l:
-            run(x, path)
-    else:
-        for i in range(5):
-            try:
-                path = make_path()
-                cmd = f'python3 python/single_eval.py "{config}" {path}'
-                subprocess.run(cmd, shell=True, check=True)
-                break
-            except subprocess.CalledProcessError as e:
-                print(e.output)
-                subprocess.run("pkill -f chrome", shell=True)
-                if os.path.exists(path):
-                    shutil.rmtree(path)
+# def run(config, in_path):
+#     def make_path():
+#         path = in_path.joinpath(time.strftime("%Y-%m-%d-%H-%M-%S"))
+#         path.mkdir()
+#         with open(path.joinpath("cfg"), "w") as f:
+#             f.write(str(config))
+#         return path
+#     if has_meta(config):
+#         path = make_path()
+#         for x in strip_quote(flatten_nondet(config)).l:
+#             run(x, path)
+#     else:
+#         for i in range(5):
+#             try:
+#                 path = make_path()
+#                 cmd = f'python3 python/single_eval.py "{config}" {path}'
+#                 subprocess.run(cmd, shell=True, check=True)
+#                 break
+#             except subprocess.CalledProcessError as e:
+#                 print(e.output)
+#                 subprocess.run("pkill -f chrome", shell=True)
+#                 if os.path.exists(path):
+#                     shutil.rmtree(path)
 
-run(NONDET(*evaluation), Path("log"))
+# run(NONDET(*evaluation), Path("log"))
 
 # print(eval_jetstream)
 
-# def full_expand(x):
-#     return flatten_nondet(strip_quote(x))
+def make_path(in_path):
+    path = in_path.joinpath(time.strftime("%Y-%m-%d-%H-%M-%S"))
+    path.mkdir()
+    return path
 
-# print(full_expand(eval_jetstream))
+cfgs = []
+
+def flatten_config(cfg):
+    if has_meta(cfg):
+        for x in strip_quote(flatten_nondet(cfg)).l:
+            flatten_config(x)
+    else:
+        cfgs.append(cfg)
+
+flatten_config(eval_jetstream)
+
+
+def run(log_path):
+    for (idx, cfg) in enumerate(cfgs):
+        exp_path = log_path.joinpath(str(idx))
+        exp_path.mkdir()
+        with open(exp_path.joinpath("cfg"), "w") as f:
+                f.write(str(cfg))
+        cmd = f'python3 python/single_eval.py "{cfg}" {exp_path}'
+        subprocess.run(cmd, shell=True, check=True)
+        print(str(idx) + " " + str(cfg))
+
+def get_dirs(path):
+    dirs = glob.glob(path+"/*/")
+    return dirs
+
+
+def get_values_in(filename, key):
+    res = []
+    with open(filename) as f:
+        for line in f.readlines():
+            j = json.loads(line)
+            # major_gc_time = j["total_major_gc_time"]
+            res.append(j[key])
+    return res
+    
+
+def compute_values(gc_file, mem_file):
+    x = get_values_in(mem_file, "Limit")
+    y = get_values_in(gc_file, "total_major_gc_time")
+    return (x, y)
+
+
+def read_cfg(dir):
+    cfg = {}
+    
+    with open(os.path.join(dir, "cfg")) as f:
+        cfg = json.load(f)
+    return cfg
+
+
+# res = [yg:{x: [val], y: [val]}, classic:{x: [val], y: [val]}, ignore:{x: [val], y: [val]}]]
+def eval_jetstream(benchmark, root_dir):
+    dirs = get_dirs(root_dir)
+    result = []
+    for dir in dirs:
+        cfg = read_cfg(dir)
+        if cfg['CFG']['BENCH'] != benchmark:
+            continue
+        balance_type = cfg['CFG']['BALANCER_CFG']['BALANCE_STRATEGY']
+        gc_file = glob.glob('*.gc.log')[0]
+        mem_file = glob.glob('*.memory.log')[0]
+        x, y = compute_values(gc_file, mem_file)
+        result[balance_type]['x'].append(x)
+        result[balance_type]['y'].append(y)
+
+    return result
+
+result = eval_jetstream("box2d.js", root_dir)
+print(result)
+    
+# log_path = make_path(Path("log"))
+
+# run()
+
+
+
+
 
 
